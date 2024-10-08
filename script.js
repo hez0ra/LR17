4d9516081b05d310e37ecd9d91549f6e0c30c78f
hez0ra <feleepen@gmail.com>
2024-10-08 09:51:10 +0300

    all done

--- script.js
+++ script.js
@@ -25,19 +25,22 @@ function firstTask(){
 }
 
 function secondTask(){
+    const imgURL1 = "https://hips.hearstapps.com/hmg-prod/images/german-armay-armoured-corps-a-heavy-tank-vi-tiger-front-news-photo-549025205-1545336787.jpg"
+    const imgURL2 = "https://waralbum.ru/wp-content/uploads/2020/11/1.png"
+
     const h1 = document.createElement('h1')
     h1.append('Второе задание')
     container.append(h1)
     
     const img = document.createElement('img')
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Tiger_BTM.jpg/1200px-Tiger_BTM.jpg'
+    img.src = imgURL1
     container.append(img)
 
     const button = document.createElement('button')
     button.append('Поменять картинку')
     button.onclick = () => {
-        if(img.src == "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Tiger_BTM.jpg/1200px-Tiger_BTM.jpg") img.src = 'https://waralbum.ru/wp-content/uploads/2020/11/1.png'
-        else img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Tiger_BTM.jpg/1200px-Tiger_BTM.jpg'
+        if(img.src == imgURL1) img.src = imgURL2
+        else img.src = imgURL1
     }
     container.append(button)
 
@@ -67,33 +70,52 @@ function thirdTask(){
 }
 
 function forthTask(){
+    const buttonWidth = "300px"
+
     const h1 = document.createElement('h1')
     h1.append('Четвертое задание')
     container.append(h1)
 
     const input = document.createElement('input')
+    input.disabled = true
     container.append(input)
 
     const buttonPlus = document.createElement('button')
-    buttonPlus.style.width = "100px"
+    buttonPlus.style.width = buttonWidth
     buttonPlus.append("Я добавлю +")
+    buttonPlus.onclick = () => input.value += "+"
     container.append(buttonPlus)
 
     const buttonMinus = document.createElement('button')
-    buttonMinus.style.width = "100px"
+    buttonMinus.style.width = buttonWidth
     buttonMinus.append("Я добавлю -")
+    buttonMinus.onclick = () => input.value += "-"
     container.append(buttonMinus)
 
     const buttonDivide = document.createElement('button')
-    buttonDivide.style.width = "100px"
+    buttonDivide.style.width = buttonWidth
     buttonDivide.append("Я добавлю /")
+    buttonDivide.onclick = () => input.value += "/"
     container.append(buttonDivide)
 
     const buttonMultiply = document.createElement('button')
-    buttonMultiply.style.width = "100px"
+    buttonMultiply.style.width = buttonWidth
+    buttonMultiply.style.marginBottom = "10px"
     buttonMultiply.append("Я добавлю *")
+    buttonMultiply.onclick = () => input.value += "*"
     container.append(buttonMultiply)
 
+    for(let i = 1; i <= 10; i++){
+        const addNumber = document.createElement('button')
+        if(i == 10) addNumber.append(0)
+        else{
+        addNumber.append(i)
+        }
+        addNumber.style.width = buttonWidth
+        addNumber.style.margin = "2px"
+        addNumber.onclick = () => input.value += i
+        container.append(addNumber)
+    }
 }
 
 firstTask();
